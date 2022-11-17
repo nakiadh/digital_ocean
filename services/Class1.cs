e576789a62e5d18e5d8bb50c8921f48d04a5d27e
Nakia Hargrave <ndhargrave1@buffs.wtamu.edu>
2022-11-16 22:07:57 -0600

    added dependencies between three projects

--- services/Class1.cs
+++ services/Class1.cs
@@ -1,5 +1,8 @@
 ﻿namespace services;
 public class Class1
 {
-
+    public int TheAdder (int x, int y)
+    {
+        return x + y;
+    }
 }
--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,10 +1,18 @@
 namespace tests;
+using services;
 
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
+        //Arrange
+        Class1 cl = new Class1();
 
+        //Act
+        int z = cl.TheAdder(0, 1);
+
+        //Assert
+        Assert.Equal(z, 1);
     }
 }
